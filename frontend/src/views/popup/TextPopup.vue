b406a16a3447f9f3bf73f3ee6921f1b20a914e4a
minjae <alswo2722@naver.com>
2019-03-18 00:13:24 +0900

    드디어 끝이 보인다

--- frontend/src/views/popup/TextPopup.vue
+++ frontend/src/views/popup/TextPopup.vue
@@ -19,7 +19,12 @@ export default {
                 { background: '#A69F82', color:'#334C54' },
                 { background: '#3788BC', color:'#F5C62E' },
                 { background: '#FEF6E8', color:'#E57F7C' },
-                { background: '#32435F', color:'#EB9772' }
+                { background: '#32435F', color:'#EB9772' },
+                { background: '#EAA932', color:'#C23B2A' },
+                { background: '#334C54', color:'#A69F82' },
+                { background: '#F5C62E', color:'#3788BC' },
+                { background: '#E57F7C', color:'#FEF6E8' },
+                { background: '#EB9772', color:'#32435F' }
             ],
             background: '#C23B2A',
             color: '#EAA932'
@@ -33,7 +38,7 @@ export default {
             this.$emit('startEvent')
         },
         randomColor() {
-            var random = Math.floor(Math.random() * 5)
+            var random = Math.floor(Math.random() * 10)
             this.background = this.color_arr[random].background
             this.color = this.color_arr[random].color
         }
